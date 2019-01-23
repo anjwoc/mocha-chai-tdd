da1a1cd91040c8e2db1a5499f8a76acd6055305b
anjwoc <anjwoc@naver.com>
2019-01-23 21:52:09 +0900

    코드 리팩토링 적용 후

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var user = require('./api/user/index.js');
 
+if(process.env.NODE_ENV !== 'test'){
+  //package.json에서 NODE_ENV=test를 설정해뒀는데
+  //test는 process객체 안으로 들어가서 test라는 환경이 아닐때만 로그가 찍히도록
+  //morgan설정
+  app.use(morgan('dev'));
+}
 
-app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/users', user);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
 
 module.exports = app;
